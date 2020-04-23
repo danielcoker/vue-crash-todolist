ff3d1ef25dc7d6514fc24429cd4a5254823ba03a
Daniel Coker <36392491+danielcoker@users.noreply.github.com>
2020-04-23 02:55:11 +0100

    Create todolist app

---
+++ src/views/About.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>
+      This is the TodoList App v1.0.0. It is part of the Vue crash course on
+      YouTube
+    </p>
+  </div>
+</template>
